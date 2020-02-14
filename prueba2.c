7704fb3d1e89508fefb869d2a9fddca6afe0f7d2
Michelle Bloomfield <blo16803@uvg.edu.gt>
2020-02-14 09:53:06 -0600

    changes

---
+++ prueba2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+void divisible(char* prog_name);
+double f(double x);    /* Function we're integrating */
+void Trap(double a, double b, int n, double* global_result_p);
+
+int main(int argc, char* argv[]) {
+   double  global_result = 0.0;  /* Store result in global_result */
+   double  a, b;                 /* Left and right endpoints      */
+   int     n;                    /* Total number of trapezoids    */
+   int     thread_count;
+
+   if (argc != 5) divisible(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   printf("Enter a, b, and n\n");
+   scanf("%lf %lf %d", &a, &b, &n);
+
+   if (n % thread_count != 0) divisible(argv[0]); //si n es divisible por thread count
+#  pragma omp parallel num_threads(thread_count) 
+   Trap(a, b, n, &global_result);
+
+   printf("Con el contador de threads = %d threads, el estimado", thread_count);
+   printf("DIVISIBLE: n = %d trapezoids, el estimado\n", n);
+   printf("of the integral from %f to %f = %.14e\n",
+      a, b, global_result);
+   return 0;
+}  /* main */
+
+/*------------------------------------------------------------------
+ * Function:    f
+ * Purpose:     Compute value of function to be integrated
+ * Input arg:   x
+ * Return val:  f(x)
+ */
+double f(double x) {
+   double return_val;
+
+   return_val = x*x;
+   return return_val;
+}  /* f */
+
+void divisible(char*prog_name){
+   fprintf(stderr, "usage: %s numero de trapezoides debe ser divisible por numero de threads \n", prog_name);
+   exit(0);
+}
+
+/*------------------------------------------------------------------
+ * Function:    Trap
+ * Purpose:     Use trapezoidal rule to estimate definite integral
+ * Input args:  
+ *    a: left endpoint
+ *    b: right endpoint
+ *    n: number of trapezoids
+ * Output arg:
+ *    integral:  estimate of integral from a to b of f(x)
+ */
+void Trap(double a, double b, int n, double* global_result_p) {
+   double  h, x, my_result;
+   double  local_a, local_b;
+   int  i, local_n;
+   int my_rank = omp_get_thread_num();
+   int thread_count = omp_get_num_threads();
+
+   h = (b-a)/n; 
+   local_n = n/thread_count;  
+   local_a = a + my_rank*local_n*h; 
+   local_b = local_a + local_n*h; 
+   my_result = (f(local_a) + f(local_b))/2.0; 
+   for (i = 1; i <= local_n-1; i++) {
+     x = local_a + i*h;
+     my_result += f(x);
+   }
+   my_result = my_result*h; 
+
+//#  pragma omp critical 
+   *global_result_p += my_result; 
+}  /* Trap */
